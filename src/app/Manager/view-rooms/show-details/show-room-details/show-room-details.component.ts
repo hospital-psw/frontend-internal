b36fde1e95fc0259da4814e16d4d53992b5b98bc
BrankaKljajic <branka.kljajic00@gmail.com>
2022-11-01 15:59:22 +0100

    Added validation for working hours

--- src/app/Manager/view-rooms/show-details/show-room-details/show-room-details.component.ts
+++ src/app/Manager/view-rooms/show-details/show-room-details/show-room-details.component.ts
@@ -45,41 +45,47 @@ export class ShowRoomDetailsComponent implements OnInit {
     
     e.preventDefault();
 
+    if(newStart.includes(":") && newEnd.includes(":") && newStart.length <= 5 && newEnd.length <= 5){
+      let splited = newStart.split(":", 10);
+      let hourStart = parseInt(splited[0]);
+      let minuteStart = parseInt(splited[1])
+      const start1 = new Date(2022, 10, 10, hourStart, minuteStart)
+      //let converted = new Date(start1.getUTCFullYear(), start1.getUTCMonth(), start1.getUTCDate(), start1.getUTCHours(), start1.getUTCMinutes(), start1.getUTCSeconds())
+      console.log("novo start:" + start1);
+      //let convertedStart = moment.utc(start1).local().format('YYYY-MM-DDTHH:mm:SS:ss');
+      let datum = new Date(start1.getTime() - (start1.getTimezoneOffset() * 60000))
     
-
-    let splited = newStart.split(":", 10);
-    let hourStart = parseInt(splited[0]);
-    let minuteStart = parseInt(splited[1])
-    const start1 = new Date(2022, 10, 10, hourStart, minuteStart)
-    //let converted = new Date(start1.getUTCFullYear(), start1.getUTCMonth(), start1.getUTCDate(), start1.getUTCHours(), start1.getUTCMinutes(), start1.getUTCSeconds())
-    console.log("novo start:" + start1);
-    //let convertedStart = moment.utc(start1).local().format('YYYY-MM-DDTHH:mm:SS:ss');
-    let datum = new Date(start1.getTime() - (start1.getTimezoneOffset() * 60000))
+      let splitedEnd = newEnd.split(":", 10);
+      let hourEnd = parseInt(splitedEnd[0])
+      let minuteEnd = parseInt(splitedEnd[1])
+      const end1 = new Date(2022, 10, 10, hourEnd, minuteEnd)
+      let datum2 = new Date(end1.getTime() - (end1.getTimezoneOffset() * 60000))
+      let num = parseInt(newNumber)
   
-    let splitedEnd = newEnd.split(":", 10);
-    let hourEnd = parseInt(splitedEnd[0])
-    let minuteEnd = parseInt(splitedEnd[1])
-    const end1 = new Date(2022, 10, 10, hourEnd, minuteEnd)
-    let datum2 = new Date(end1.getTime() - (end1.getTimezoneOffset() * 60000))
-    let num = parseInt(newNumber)
-
-    const updatedWorkingHours: IWorkingHours = {id: this.room.workingHours.id,
-                                        start: datum,
-                                        end: datum2}
-                                        console.log("novo radno vreme", updatedWorkingHours)
-    const updatedRoom: IRoom = {id: this.room.id,
-                                number: newNumber,
-                                floor: this.room.floor,
-                                purpose: newPurpose,
-                                workingHours: updatedWorkingHours
-                                }
-    this.roomService.editRoom(updatedRoom).subscribe(
-      {next:(res) =>{
-        this.showSuccess();
-      },
-      error:(e) => {this.showError()}
+      const updatedWorkingHours: IWorkingHours = {id: this.room.workingHours.id,
+                                          start: datum,
+                                          end: datum2}
+                                          console.log("novo radno vreme", updatedWorkingHours)
+      const updatedRoom: IRoom = {id: this.room.id,
+                                  number: newNumber,
+                                  floor: this.room.floor,
+                                  purpose: newPurpose,
+                                  workingHours: updatedWorkingHours
+                                  }
+      this.roomService.editRoom(updatedRoom).subscribe(
+        {next:(res) =>{
+          this.showSuccess();
+         },
+        error:(e) => {this.showError()}
+      }
+      );
     }
-    );
+    this.showError();
+
+    let format = newStart.includes(":")
+    console.log("format",format)
+
+    
   }
 
   showError() {
