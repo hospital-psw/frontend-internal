413b33e86ef9597c6537a65932d2a3f11b71b490
Ana Vulin <vulin.ra65.2019@uns.ac.rs>
2022-11-09 00:33:29 +0100

    Added information is request urgent

--- src/app/Manager/service/vacation-requests.service.ts
+++ src/app/Manager/service/vacation-requests.service.ts
@@ -39,7 +39,7 @@ export class VacationRequestsService {
     /*return this.http.get<IVacationRequest[]>(
       `http://localhost:16177/api/map/getVacationRequests`
     );*/
-    this.vacationRequests.push({doctor: {id: 1, firstName: "Ana", lastName: "Vulin", "email": "lala"}, from: new Date(), to: new Date(), urgent: false, status: "pending", "comment": "aaaaaa", "managerComment": ""});
+    this.vacationRequests.push({doctor: {id: 1, firstName: "Ana", lastName: "Vulin", "email": "lala"}, from: new Date(), to: new Date(), urgent: true, status: "pending", "comment": "aaaaaa", "managerComment": ""});
     return this.vacationRequests;
 >>>>>>> 74a2ecb (Implemented UI for showing requests and modal dialog for request rejection)
   }
