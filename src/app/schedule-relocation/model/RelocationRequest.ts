1872d4aa2980d9ed2ad5fe3f6f83ea789fd200a4
sara1404 <sara14042000@gmail.com>
2022-11-09 21:20:28 +0100

    model for relocation request made

---
+++ src/app/schedule-relocation/model/RelocationRequest.ts
@@ -0,0 +1,9 @@
+// import { IRoom } from "src/app/Manager/Model/Room";
+
+// export interface IRelocationRequest{
+//   fromRoom: IRoom,
+//   toRoom: IRoom,
+//   startTime: Date,
+//   duration: number,
+//   equipment:
+// }
