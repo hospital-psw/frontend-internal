5593244942aeaf62b4bb7e4b4973494e9a9f7afc
Ana Vulin <vulin.ra65.2019@uns.ac.rs>
2022-11-11 20:31:55 +0100

    Small change

--- src/app/Manager/Model/VacationRequest.ts
+++ src/app/Manager/Model/VacationRequest.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Doctor } from '../../schedule/interface/Doctor';
 
 export interface IVacationRequest {
@@ -11,16 +10,4 @@ export interface IVacationRequest {
   urgent: boolean;
   managerComment: string;
 }
-=======
-import { IUser } from "./User";
 
-export interface IVacationRequest {
-    doctor : IUser;
-    from : Date;
-    to : Date;
-    status: string;
-    comment: string;
-    urgent: boolean;
-    managerComment: string;
-}
->>>>>>> 74a2ecb (Implemented UI for showing requests and modal dialog for request rejection)
