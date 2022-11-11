0c45e1d7c912fca87b9e6939ddb94aef7486c93d
Ana Vulin <vulin.ra65.2019@uns.ac.rs>
2022-11-11 20:31:55 +0100

    Small change

--- src/app/Manager/service/vacation-requests.service.ts
+++ src/app/Manager/service/vacation-requests.service.ts
@@ -3,14 +3,13 @@ import { HttpClient } from '@angular/common/http';
 import { IVacationRequest } from '../Model/VacationRequest';
 import { Observable } from 'rxjs';
 
-
 @Injectable({
   providedIn: 'root',
 })
 export class VacationRequestsService {
   constructor(private http: HttpClient) {}
 
-  getVacationRequests(): Observable<IVacationRequest[]> {
+  getVacationRequests() : Observable<IVacationRequest[]> {
     return this.http.get<IVacationRequest[]>(
       `http://localhost:16177/api/VacationRequests/getAllPending`
     );
