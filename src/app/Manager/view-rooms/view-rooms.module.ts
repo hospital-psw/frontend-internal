c5bc6413dae2d3d963ff1306baf6d74c7f635406
Sandra <sandra.brkic5@gmail.com>
2022-11-13 09:38:14 +0100

    Fix style for search

--- src/app/Manager/view-rooms/view-rooms.module.ts
+++ src/app/Manager/view-rooms/view-rooms.module.ts
@@ -15,6 +15,7 @@ import { ShowRoomDetailsComponent } from './show-details/show-room-details/show-
 import { ShowSearchedRoomsComponent } from './show-searched-rooms/show-searched-rooms/show-searched-rooms.component';
 import { ShowEquipmentComponent } from './show-details/show-equipment/show-equipment.component';
 import { MatTableModule } from '@angular/material/table';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -32,6 +33,7 @@ import { MatTableModule } from '@angular/material/table';
     MatOptionModule,
     MatInputModule,
     MatTableModule,
+    MatButtonModule
   ],
   exports: [ViewRoomsComponent],
 })
