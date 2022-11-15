ec8fea0c9fbb9574d8cf6b928eaadbc10207c5c3
Vuk Milanovic <vuk.milanovic11@gmial.com>
2022-11-15 20:18:59 +0100

    create-treatment components added

--- src/app/medical-treatment/medical-treatment.module.ts
+++ src/app/medical-treatment/medical-treatment.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CreateTreatmentBaseComponent } from './create-medical-treatment/create-treatment-base/create-treatment-base.component';
+import { CreateTreatmentBaseComponent } from './create-medical-treatment/base-create-treatment/create-treatment-base.component';
 import { CreateTreatmentFormComponent } from './create-medical-treatment/create-treatment-form/create-treatment-form.component';
 
 @NgModule({
   declarations: [CreateTreatmentBaseComponent, CreateTreatmentFormComponent],
   imports: [CommonModule],
 })
-export class MedicalTreatmentModule {}
+export class MedicalTreatmentModule { }
