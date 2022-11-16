058b91ddb94c3f2a270bbca32f83869c888eda95
sara1404 <sara14042000@gmail.com>
2022-11-16 16:28:03 +0100

    fix

--- src/app/Manager/view-vacation-requests/vacation-request/request.component.ts
+++ src/app/Manager/view-vacation-requests/vacation-request/request.component.ts
@@ -1,17 +1,6 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { emitDistinctChangesOnlyDefaultValue } from '@angular/compiler';
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { MatDialog, MatDialogClose } from '@angular/material/dialog';
-=======
-import { Component, OnInit, Input } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
->>>>>>> b826a10 (Implemented UI for showing requests and modal dialog for request rejection)
-=======
-import { emitDistinctChangesOnlyDefaultValue } from '@angular/compiler';
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { MatDialog, MatDialogClose } from '@angular/material/dialog';
->>>>>>> b346f01 (handle vacation request functionality implemented)
 import { IVacationRequest } from '../../Model/VacationRequest';
 import { VacationRequestsService } from '../../service/vacation-requests.service';
 import { RejectRequestDialogComponent } from '../reject-request-dialog/reject-request-dialog.component';
@@ -19,8 +8,6 @@ import { RejectRequestDialogComponent } from '../reject-request-dialog/reject-re
 @Component({
   selector: 'app-request',
   templateUrl: './request.component.html',
-<<<<<<< HEAD
-<<<<<<< HEAD
   styleUrls: ['./request.component.scss'],
 })
 export class RequestComponent implements OnInit {
@@ -57,67 +44,6 @@ export class RequestComponent implements OnInit {
           context.notify.emit();
         });
     });
-=======
-  styleUrls: ['./request.component.scss']
-=======
-  styleUrls: ['./request.component.scss'],
->>>>>>> 1bcaf2f (small update)
-})
-export class RequestComponent implements OnInit {
-  @Input() request: IVacationRequest;
-  clickButton: boolean = false;
-  @Output() notify = new EventEmitter();
-  constructor(
-    public dialog: MatDialog,
-    private vacationRequestService: VacationRequestsService
-  ) {}
-
-  ngOnInit(): void {}
-
-  onAccept(id: number) {
-    this.clickButton = true;
-    let context = this;
-
-    this.vacationRequestService.acceptVacationRequest(id).subscribe((res) => {
-      context.notify.emit();
-    });
-  }
-
-  onReject(id: number, managerComment: string) {
-    let context = this;
-    this.clickButton = true;
-<<<<<<< HEAD
-    let context = this
-
-    this.vacationRequestService.acceptVacationRequest(id).subscribe(res => {
-      context.notify.emit()
-    })
->>>>>>> b826a10 (Implemented UI for showing requests and modal dialog for request rejection)
-  }
-
-  onReject(id: number, managerComment: string){
-    let context = this
-    this.clickButton = true;
-    let dialogRef = this.dialog.open(RejectRequestDialogComponent, { disableClose: true });
-    dialogRef.afterClosed().subscribe(result =>{
-      if(result==='canceled') return
-      this.vacationRequestService.declineVacationRequest(id, managerComment).subscribe(res => {
-        context.notify.emit()
-      })
-    })
-
-=======
-    let dialogRef = this.dialog.open(RejectRequestDialogComponent, {
-      disableClose: true,
-    });
-    dialogRef.afterClosed().subscribe((result) => {
-      if (result === 'canceled') return;
-      this.vacationRequestService
-        .declineVacationRequest(id, managerComment)
-        .subscribe((res) => {
-          context.notify.emit();
-        });
-    });
->>>>>>> 1bcaf2f (small update)
   }
 }
+
--- src/app/Manager/view-vacation-requests/vacation-request/request.module.ts
+++ src/app/Manager/view-vacation-requests/vacation-request/request.module.ts
@@ -5,28 +5,12 @@ import { MaterialModule } from 'src/app/material/material.module';
 import { RequestComponent } from './request.component';
 import { RejectRequestDialogComponent } from '../reject-request-dialog/reject-request-dialog.component';
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-@NgModule({
-  declarations: [RequestComponent, RejectRequestDialogComponent],
-  entryComponents: [RejectRequestDialogComponent],
-  imports: [CommonModule, MaterialModule],
-  exports: [RequestComponent],
-})
-export class RequestModule {}
-=======
 
-=======
->>>>>>> 1bcaf2f (small update)
 @NgModule({
   declarations: [RequestComponent, RejectRequestDialogComponent],
   entryComponents: [RejectRequestDialogComponent],
   imports: [CommonModule, MaterialModule],
   exports: [RequestComponent],
 })
-<<<<<<< HEAD
-export class RequestModule { }
->>>>>>> b826a10 (Implemented UI for showing requests and modal dialog for request rejection)
-=======
 export class RequestModule {}
->>>>>>> 1bcaf2f (small update)
+
