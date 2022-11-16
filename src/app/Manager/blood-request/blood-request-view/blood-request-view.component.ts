809349847d0ad3b683ccd0cd028ea2715edc23fc
markovicuros <101749196+markovicuros@users.noreply.github.com>
2022-11-16 19:38:00 +0100

    applyed comments from pull request

--- src/app/Manager/blood-request/blood-request-view/blood-request-view.component.ts
+++ src/app/Manager/blood-request/blood-request-view/blood-request-view.component.ts
@@ -5,13 +5,6 @@ import { BloodType } from 'src/app/blood-bank/model/blood-type.model';
 import { BloodRequest } from '../interface/blood-request';
 import { BloodRequestService } from '../services/blood-request.service';
 
-/*
-const ELEMENT_DATA: BloodRequest[] = [
-  {doctorId: "Petar", date: "2022-11-05",bloodType: BloodType.A_NEGATIVE, amount: 5, reason: 'aa'},
-  {doctorId: "Mika", date: "2022-11-05",bloodType: BloodType.AB_NEGATIVE, amount: 7, reason: 'bb'},
-  {doctorId: "Ana", date: "2022-11-05",bloodType: BloodType.O_POSITIVE, amount: 2, reason: 'cc'},
-];*/
-
 @Component({
   selector: 'app-blood-request-view',
   templateUrl: './blood-request-view.component.html',
@@ -21,7 +14,6 @@ export class BloodRequestViewComponent implements OnInit {
   STATUS: string;
 
   public dataSource = new MatTableDataSource<BloodRequest>();
-  //public dataSource = ELEMENT_DATA;
   public displayedColumns = ['doctor', 'bloodType', 'amount', 'date', 'status'];
   public bloodRequests: BloodRequest[] = [];
   constructor(private bservice: BloodRequestService, private router: Router) {}
@@ -34,7 +26,7 @@ export class BloodRequestViewComponent implements OnInit {
     });
   }
 
-  public MakeAccepted(id: number) {
+  public makeAccepted(id: number) {
     this.bservice.MakeAccepted(id).subscribe((res) => {
       this.STATUS = 'ACCEPTED';
       this.bservice.getAccepted().subscribe((res) => {
@@ -44,7 +36,7 @@ export class BloodRequestViewComponent implements OnInit {
     });
   }
 
-  public MakeDeclined(id: number) {
+  public makeDeclined(id: number) {
     this.bservice.MakeDeclined(id).subscribe((res) => {
       this.STATUS = 'DECLINED';
       this.bservice.getDeclined().subscribe((res) => {
