de8acf4bdd879020249ee89f758ed0581029bd26
DarkoCokic72 <93010785+DarkoCokic72@users.noreply.github.com>
2022-11-16 15:29:38 +0100

    Created interfaces and enums

---
+++ src/app/vacation-request/vacation-request.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { VacationRequestService } from './service/vacation-request.service';
+
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  providers: [
+    VacationRequestService
+  ]
+})
+export class VacationRequestModule { }
