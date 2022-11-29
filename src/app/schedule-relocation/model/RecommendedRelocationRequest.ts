5baf20c04c43b28abf5616dae9198307b26e3f17
Ana Vulin <vulin.ra65.2019@uns.ac.rs>
2022-11-29 23:41:56 +0100

    fixed appointments recommendation for relocation

--- src/app/schedule-relocation/model/RecommendedRelocationRequest.ts
+++ src/app/schedule-relocation/model/RecommendedRelocationRequest.ts
@@ -2,8 +2,7 @@ import { IEquipment } from 'src/app/Manager/Model/Equipment';
 import { IRoom } from 'src/app/Manager/Model/Room';
 
 export interface IRecommendedRelocationRequest {
-  fromRoomId: number;
-  toRoomId: number;
+  roomsId: number[];
   fromTime: Date;
   toTime: Date;
   duration: number;
