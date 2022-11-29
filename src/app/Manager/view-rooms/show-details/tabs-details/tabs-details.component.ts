c3510348a6aa7fb85f033af4eeab083527a031b4
Sandra <sandra.brkic5@gmail.com>
2022-11-29 16:55:45 +0100

    Move equipment table to tab

--- src/app/Manager/view-rooms/show-details/tabs-details/tabs-details.component.ts
+++ src/app/Manager/view-rooms/show-details/tabs-details/tabs-details.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
-import { OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {  OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { IAppointmentDisplay } from 'src/app/Manager/Model/AppointmentDisplay';
+import { IEquipment } from 'src/app/Manager/Model/Equipment';
 import { IRelocationRequestDisplay } from 'src/app/Manager/Model/RelocationRequestDisplay';
+import { ToastrService } from 'ngx-toastr';
+import { RelocationRequestService } from 'src/app/Manager/service/relocation-request-service';
 
 @Component({
   selector: 'app-tabs-details',
@@ -9,7 +12,40 @@ import { IRelocationRequestDisplay } from 'src/app/Manager/Model/RelocationReque
   styleUrls: ['./tabs-details.component.scss']
 })
 export class TabsDetailsComponent {
-  constructor() {}
+  constructor(private toastr: ToastrService, private relocationRequestService: RelocationRequestService) {}
   @Input() relocationRequests: IRelocationRequestDisplay[];
   @Input() appointments: IAppointmentDisplay[]
+  @Input() equipment: IEquipment[];
+
+  @Output() relocateNotify = new EventEmitter<any>();
+  element: IEquipment;
+  doRelocate: boolean = false;
+  selectedEquipment: string = '-1';
+  showSuccess() {
+    this.toastr.success('Successfully eddited room.', 'Success');
+  }
+
+  //relocate(element: IEquipment) {
+   // this.relocateNotify.emit(element);
+  //}
+
+  selectEquipment(evt: any): void {
+    this.selectedEquipment = evt.value;
+  }
+
+  relocate(element: IEquipment) {
+    this.doRelocate = true;
+    this.element = element;
+  }
+
+  refreshEquipment(event: any) {
+    this.equipment.forEach((element) => {
+      if (element.id == event.equipmentId)
+        element.reservedQuantity += event.reservedQuantity;
+    });
+  }
+
+  closeStepper() {
+    this.doRelocate = false;
+  }
 }
