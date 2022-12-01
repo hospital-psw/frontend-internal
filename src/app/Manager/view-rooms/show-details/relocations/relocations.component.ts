263f1db397a038aa52ace376fda94f747de998e7
Sandra <sandra.brkic5@gmail.com>
2022-12-01 11:54:42 +0100

    Small changes

--- src/app/Manager/view-rooms/show-details/relocations/relocations.component.ts
+++ src/app/Manager/view-rooms/show-details/relocations/relocations.component.ts
@@ -11,12 +11,20 @@ import { IRelocationRequestDisplay } from 'src/app/Manager/Model/RelocationReque
 export class RelocationsComponent {
   constructor(private roomService: RoomService){}
 
-  displayedColumns: string[] = ['from', 'to', 'equipmentType', 'quantity' ,'startTime', 'duration'];
+  displayedColumns: string[] = ['from', 'to', 'equipmentType', 'quantity' ,'startTime', 'duration']; //, 'button'
   @Input() relocationRequests: IRelocationRequestDisplay[]
   newDate: Date
   
   convertEnum(type: number): string {
     return EquipmentTypeEnum[type];
+  }
+
+  check24h(date: any):boolean{
+    const now = new Date().valueOf()
+    const relocation = new Date(date).valueOf()
+    var hours = relocation - now
+    console.log(hours)
+    return true;
   }
 
 }
