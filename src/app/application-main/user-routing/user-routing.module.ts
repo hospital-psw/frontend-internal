4813f3255243afc9ae1d01cdf390f4a71c478fdb
unknown <kalinic.ra71.2019@uns.ac.rs>
2022-12-14 21:31:57 +0100

    display tenders and fix routing

--- src/app/application-main/user-routing/user-routing.module.ts
+++ src/app/application-main/user-routing/user-routing.module.ts
@@ -27,6 +27,9 @@ import { ExaminationStepperComponent } from 'src/app/examinations/examination-st
 import { ShowTendersComponent } from 'src/app/tenders/show-tenders/show-tenders.component';
 import { CreateTenderComponent } from 'src/app/tenders/create-tender/create-tender.component';
 import { BlockPatientsViewComponent } from 'src/app/Manager/block-patients/block-patients-view/block-patients-view.component';
+import { DetailComponent } from 'src/app/blood-bank/detail/detail.component';
+import { UpdateComponent } from 'src/app/blood-bank/update/update.component';
+import { CreateComponent } from 'src/app/blood-bank/create/create.component';
 
 const routes: Routes = [
   {
@@ -138,6 +141,18 @@ const routes: Routes = [
   {
     path: 'blockpatients',
     component: BlockPatientsViewComponent,
+  },
+  {
+    path: 'bloodbank/:id/detail',
+    component: DetailComponent,
+  },
+  {
+    path: 'bloodbank/:id/update',
+    component: UpdateComponent,
+  },
+  {
+    path: 'bloodbank/add',
+    component: CreateComponent,
   },
 ];
 
