9415d17611ffc725b1ae58f8030cb21eddd784bc
BrankaKljajic <93400805+BrankaKljajic@users.noreply.github.com>
2023-01-04 17:21:56 +0100

    Ge#4.3.2 feature/tender blood type statistics (#93)
    * doctor's yearly booking statistics
    
    * Created graph for blood quantity on tender
    
    * Bug fixed
    
    * format fixed
    
    Co-authored-by: Ana Vulin <vulin.ra65.2019@uns.ac.rs>

--- src/app/Statistics/statistics/Components/statistics/statistics.component.ts
+++ src/app/Statistics/statistics/Components/statistics/statistics.component.ts
@@ -5,6 +5,7 @@ import { DataTableItem } from '../data-table/data-table-datasource';
 import { DoctorService } from '../../Services/doctor.service';
 import { ax } from 'chart.js/dist/chunks/helpers.core';
 import { isThisISOWeek, parse } from 'date-fns';
+import { TenderService } from '../../Services/tender.service';
 
 @Component({
   selector: 'app-statistics',
@@ -27,9 +28,13 @@ export class StatisticsComponent implements OnInit {
   chart6_data: any = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
   doctorYearlyBookingChart: any = [];
   doctors: any = [];
+
+  tenderBloodQuantityChart: any;
+  chart7_data: any = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
   constructor(
     private service: StatisticsService,
-    private doctorService: DoctorService
+    private doctorService: DoctorService,
+    private tenderService: TenderService
   ) {
     Chart.register(...registerables);
   }
@@ -523,6 +528,66 @@ export class StatisticsComponent implements OnInit {
         },
       },
     });
+    this.tenderBloodQuantityChart = new Chart('chart7', {
+      type: 'line',
+      data: {
+        labels: [
+          'January',
+          'February',
+          'March',
+          'April',
+          'May',
+          'June',
+          'July',
+          'August',
+          'September',
+          'October',
+          'November',
+          'December',
+        ],
+        datasets: [
+          {
+            label: 'Quantity of blood per month',
+            data: this.chart7_data,
+            backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+            ],
+            borderWidth: 3,
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+        plugins: {
+          legend: {
+            display: false,
+          },
+          title: {
+            color: 'gray',
+            display: true,
+            font: {
+              size: 20,
+            },
+            text: 'Quantity of blood per month',
+            padding: {
+              top: 10,
+            },
+          },
+        },
+      },
+    });
   }
   getVacationStatistic(event: any) {
     //pozvati funkciju za dobijanje podataka i proslediti event.value
@@ -874,5 +939,86 @@ export class StatisticsComponent implements OnInit {
         },
       });
     });
+  }
+
+  bloodType: any = null;
+  saveBloodType(event: any) {
+    this.bloodType = parseInt(event.value);
+    this.getQuantityOfBloodPerMonth();
+  }
+  year1: any = null;
+  saveYear1(event: any) {
+    this.year1 = parseInt(event.value);
+    this.getQuantityOfBloodPerMonth();
+  }
+
+  getQuantityOfBloodPerMonth() {
+    if (this.year1 == null || this.bloodType == null) return;
+    this.tenderBloodQuantityChart.destroy();
+    this.tenderService
+      .getQuantityOfBloodPerMonth(this.year1, this.bloodType)
+      .subscribe((data) => {
+        this.chart7_data = data;
+        this.tenderBloodQuantityChart = new Chart('chart7', {
+          type: 'line',
+          data: {
+            labels: [
+              'January',
+              'February',
+              'March',
+              'April',
+              'May',
+              'June',
+              'July',
+              'August',
+              'September',
+              'October',
+              'November',
+              'December',
+            ],
+            datasets: [
+              {
+                label: 'Quantity of blood per month',
+                data: this.chart7_data,
+                backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+                borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)',
+                ],
+                borderWidth: 3,
+              },
+            ],
+          },
+          options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+              y: {
+                beginAtZero: true,
+              },
+            },
+            plugins: {
+              legend: {
+                display: false,
+              },
+              title: {
+                color: 'gray',
+                display: true,
+                font: {
+                  size: 20,
+                },
+                text: 'Quantity of blood per month',
+                padding: {
+                  top: 10,
+                },
+              },
+            },
+          },
+        });
+      });
   }
 }
