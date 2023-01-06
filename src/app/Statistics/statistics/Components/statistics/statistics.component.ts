a518fa2683f5fc9def3c3cc75f1e90236c6e330c
sara1404 <sara14042000@gmail.com>
2023-01-06 16:33:31 +0100

    linted

--- src/app/Statistics/statistics/Components/statistics/statistics.component.ts
+++ src/app/Statistics/statistics/Components/statistics/statistics.component.ts
@@ -21,21 +21,18 @@ export class StatisticsComponent implements OnInit {
   max: number = 0;
   tableData: DataTableItem[] = [];
   chart5_data: any = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-  vacationsChart : any
+  vacationsChart: any;
 
   chart6_data: any = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-  doctorYearlyBookingChart : any = []
+  doctorYearlyBookingChart: any = [];
   doctors: any = [];
 
-
-  yearView = false
-  monthlyView = false
-  optionalView = false
-
+  yearView = false;
+  monthlyView = false;
+  optionalView = false;
 
   chartTenderMoneyData: any = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
   tenderMoneyChart: any = [];
-
 
   tenderBloodQuantityChart: any;
   chart7_data: any = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
@@ -669,111 +666,159 @@ export class StatisticsComponent implements OnInit {
     });
   }
 
-  year : any = null
-  doctor : any = null
-  month: any = null
+  year: any = null;
+  doctor: any = null;
+  month: any = null;
 
-  saveYear(event: any){
-    this.year = parseInt(event.value)
-    this.getDoctorYearlyBookingStatistic()
-  } 
+  saveYear(event: any) {
+    this.year = parseInt(event.value);
+    this.getDoctorYearlyBookingStatistic();
+  }
   saveDoctor(event: any) {
-    this.doctor = event.value
-    this.getDoctorYearlyBookingStatistic()
+    this.doctor = event.value;
+    this.getDoctorYearlyBookingStatistic();
   }
-  saveMonth(event: any){
-    this.month = parseInt(event.value)
-    this.getDoctorMonthlyBookingStatistic()
+  saveMonth(event: any) {
+    this.month = parseInt(event.value);
+    this.getDoctorMonthlyBookingStatistic();
   }
 
-  getLabelsBasedOnMonth(month: number){
-    switch(month){
-      case 1: return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
-      case 2: return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28]
-      case 3: return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
-      case 4: return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
-      case 5: return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
-      case 6: return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
-      case 7: return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
-      case 8: return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
-      case 9: return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
-      case 10: return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
-      case 11: return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
-      case 12: return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
+  getLabelsBasedOnMonth(month: number) {
+    switch (month) {
+      case 1:
+        return [
+          1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+          21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+        ];
+      case 2:
+        return [
+          1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+          21, 22, 23, 24, 25, 26, 27, 28,
+        ];
+      case 3:
+        return [
+          1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+          21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+        ];
+      case 4:
+        return [
+          1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+          21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+        ];
+      case 5:
+        return [
+          1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+          21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+        ];
+      case 6:
+        return [
+          1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+          21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+        ];
+      case 7:
+        return [
+          1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+          21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+        ];
+      case 8:
+        return [
+          1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+          21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+        ];
+      case 9:
+        return [
+          1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+          21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+        ];
+      case 10:
+        return [
+          1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+          21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+        ];
+      case 11:
+        return [
+          1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+          21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+        ];
+      case 12:
+        return [
+          1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+          21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+        ];
     }
-    return []
+    return [];
   }
 
-  getDoctorMonthlyBookingStatistic(){
-    if (this.year == null || this.doctor == null || this.month == null) return 
+  getDoctorMonthlyBookingStatistic() {
+    if (this.year == null || this.doctor == null || this.month == null) return;
     this.doctorYearlyBookingChart.destroy();
-    this.service.getDoctorMonthlyBookingStatistics(this.doctor, this.month, this.year).subscribe((data) => {
-      this.chart6_data = data;
-      this.doctorYearlyBookingChart = new Chart('chart6', {
-        type: 'line',
-        data: {
-          labels: this.getLabelsBasedOnMonth(this.month),
-          datasets: [
-            {
-              label: 'Number of appointments per month',
-              data: this.chart6_data,
-              backgroundColor: ['rgba(255, 99, 132, 0.2)'],
-              borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-              ],
-              borderWidth: 3,
-            },
-          ],
-        },
-        options: {
-          responsive: true,
-          maintainAspectRatio: false,
-          scales: {
-            y: {
-              beginAtZero: true,
-            },
-          },
-          plugins: {
-            legend: {
-              display: false,
-            },
-            title: {
-              color: 'gray',
-              display: true,
-              font: {
-                size: 20,
+    this.service
+      .getDoctorMonthlyBookingStatistics(this.doctor, this.month, this.year)
+      .subscribe((data) => {
+        this.chart6_data = data;
+        this.doctorYearlyBookingChart = new Chart('chart6', {
+          type: 'line',
+          data: {
+            labels: this.getLabelsBasedOnMonth(this.month),
+            datasets: [
+              {
+                label: 'Number of appointments per month',
+                data: this.chart6_data,
+                backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+                borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)',
+                ],
+                borderWidth: 3,
               },
-              text: 'Number of appointments per month',
-              padding: {
-                top: 10,
+            ],
+          },
+          options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+              y: {
+                beginAtZero: true,
               },
             },
+            plugins: {
+              legend: {
+                display: false,
+              },
+              title: {
+                color: 'gray',
+                display: true,
+                font: {
+                  size: 20,
+                },
+                text: 'Number of appointments per month',
+                padding: {
+                  top: 10,
+                },
+              },
+            },
           },
-        },
+        });
       });
-    });
   }
 
-  saveOption(evt: any){
-    if(evt.value === 'YEARLY'){
-      this.yearView = true
-      this.monthlyView = false
-      this.optionalView = false
-    }
-    else if(evt.value === 'MONTHLY'){
-      this.yearView = false
-      this.monthlyView = true
-      this.optionalView = false
-    }
-    else if(evt.value === 'OPTIONAL'){
-      this.yearView = false
-      this.monthlyView = false
-      this.optionalView = true
+  saveOption(evt: any) {
+    if (evt.value === 'YEARLY') {
+      this.yearView = true;
+      this.monthlyView = false;
+      this.optionalView = false;
+    } else if (evt.value === 'MONTHLY') {
+      this.yearView = false;
+      this.monthlyView = true;
+      this.optionalView = false;
+    } else if (evt.value === 'OPTIONAL') {
+      this.yearView = false;
+      this.monthlyView = false;
+      this.optionalView = true;
     }
   }
 
