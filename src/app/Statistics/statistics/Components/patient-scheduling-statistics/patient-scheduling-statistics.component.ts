2b283c7db4b104660fd18e130e8217278f7cee5e
tkrgovic <tasakrgovic@gmail.com>
2023-01-12 10:23:54 +0100

    Final changes

--- src/app/Statistics/statistics/Components/patient-scheduling-statistics/patient-scheduling-statistics.component.ts
+++ src/app/Statistics/statistics/Components/patient-scheduling-statistics/patient-scheduling-statistics.component.ts
@@ -228,16 +228,18 @@ export class PatientSchedulingStatisticsComponent {
             label: 'Average time spent on each step',
             data: this.timeSpentOnEachStepChartData,
             backgroundColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
+              
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
             ],
             borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
+              
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
             ],
             borderWidth: 3,
           },
