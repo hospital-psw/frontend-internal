6a104f71427e9b38f8e781162152710dbc8caf0c
BrankaKljajic <branka.kljajic00@gmail.com>
2023-01-14 18:11:58 +0100

    Changed css

--- src/app/EXAMPLE/display/display.component.ts
+++ src/app/EXAMPLE/display/display.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-display',
   templateUrl: './display.component.html',
-  styleUrls: ['./display.component.scss']
+  styleUrls: ['./display.component.scss'],
 })
-export class DisplayComponent {
-
-}
+export class DisplayComponent {}
