9da20f09ffd088cb6eeeed0a21afb6f7ebc883ea
unknown <kalinic.ra71.2019@uns.ac.rs>
2023-01-14 18:40:15 +0100

    small fix on feature for create tender

--- src/app/tenders/show-tenders/show-tenders.component.ts
+++ src/app/tenders/show-tenders/show-tenders.component.ts
@@ -6,6 +6,7 @@ import { Router } from '@angular/router';
 import { BloodType } from '../../blood-bank/model/blood-type.model';
 import { MatDialog } from '@angular/material/dialog';
 import { TenderReportComponent } from '../tender-report/tender-report.component';
+import { CreateTenderComponent } from '../create-tender/create-tender.component';
 
 @Component({
   selector: 'app-show-tenders',
@@ -44,7 +45,7 @@ export class ShowTendersComponent {
   }
 
   createTenderPage() {
-    this.router.navigateByUrl('/app/create-tender');
+    this.dialogRef.open(CreateTenderComponent);
   }
 
   accept(tenderId: Number, i: Number) {
